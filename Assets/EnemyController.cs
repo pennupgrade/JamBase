790c931496a1717a8464cd2e96be61b29d5e99f9
ClaireL21 <53claire@gmail.com>
2022-10-30 01:52:21 -0400

    Multiple doors light functionality added
    (TITLE) YIPPPPPEEEEE

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,46 +4,57 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
     [SerializeField] GameObject light1;
+    [SerializeField] GameObject light2;
+    [SerializeField] GameObject light3;
+    [SerializeField] GameObject light4;
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] GameObject leftSpawn;
     [SerializeField] GameObject rightSpawn;
     [SerializeField] GameObject player;
     private Queue<GameObject> enemyQueue;
-    private GameObject firstEnemy; 
+    private GameObject firstEnemy;
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
         enemyQueue = new Queue<GameObject>();
-            enemyQueue.Enqueue(GameObject.Instantiate(enemyPrefab, leftSpawn.transform.position, Quaternion.identity, this.transform));
-            firstEnemy = enemyQueue.Dequeue();
-     
+        enemyQueue.Enqueue(GameObject.Instantiate(enemyPrefab, leftSpawn.transform.position, Quaternion.identity, this.transform));
+        firstEnemy = enemyQueue.Dequeue();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.N)) {
             enemyQueue.Enqueue(GameObject.Instantiate(enemyPrefab, leftSpawn.transform.position, Quaternion.identity, this.transform));
         }
-        if (light1.GetComponent<LightAlert>().lightIsOn() 
-             && !firstEnemy.GetComponent<EnemyPatrol>().enemyIsSeeking() && !light1.GetComponent<LightAlert>().getLightHasFinder())
+
+        lightQueue(light1);
+        lightQueue(light2);
+        lightQueue(light3);
+        lightQueue(light4);
+        BroadcastMessage("CanSee", player.GetComponent<PlayerMovement>().getIsHiding());
+
+
+    }
+
+    private void lightQueue(GameObject light) {
+        if (light.GetComponent<LightAlert>().lightIsOn()
+             && !firstEnemy.GetComponent<EnemyPatrol>().enemyIsSeeking() && !light.GetComponent<LightAlert>().getLightHasFinder())
         {
-            firstEnemy.GetComponent<EnemyPatrol>().startSeeking(light1);
-            light1.GetComponent<LightAlert>().toggleLightHasFinder();
+            firstEnemy.GetComponent<EnemyPatrol>().startSeeking(light);
+            light.GetComponent<LightAlert>().toggleLightHasFinder();
             if (enemyQueue.Count >= 1)
             {
                 firstEnemy = enemyQueue.Dequeue();
             }
-            else {
+            else
+            {
                 enemyQueue.Enqueue(GameObject.Instantiate(enemyPrefab, leftSpawn.transform.position, Quaternion.identity, this.transform));
                 firstEnemy = enemyQueue.Dequeue();
             }
         }
-
-        BroadcastMessage("CanSee", player.GetComponent<PlayerMovement>().getIsHiding());
-
-
     }
 }
